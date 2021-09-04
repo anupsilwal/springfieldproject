bc2437872ac886a8f405db257b2156696d94e92e
Anup Silwal <silwalanup6@gmail.com>
2021-09-04 22:58:07 +1000

    finish asssignment Springfield project

---
+++ admin/order_master_detail.php
@@ -0,0 +1,62 @@
+<?php
+require('top.inc.php');
+$order_id=get_safe_value($con,$_GET['id']);
+if(isset($_POST['update_order_status'])){
+	$update_order_status=$_POST['update_order_status'];
+	if($update_order_status=='5'){
+		mysqli_query($con,"update `order` set order_status='$update_order_status',payment_status='Success' where id='$order_id'");
+	}else{
+		mysqli_query($con,"update `order` set order_status='$update_order_status' where id='$order_id'");
+	}
+	header('location:order_master.php');
+	die();
+	
+}
+?>
+<div class="content pb-0">
+	<div class="orders">
+	   <div class="row">
+		  <div class="col-xl-12">
+			 <div class="card">
+				<div class="card-body">
+				   <h4 class="box-title">Order Detail </h4>
+				</div>
+				<div class="card-body--">
+				   <div class="table-stats order-table ov-h">
+					  
+						<div id="address_details">
+							<strong>Order Status</strong>
+							<?php 
+							$order_status_arr=mysqli_fetch_assoc(mysqli_query($con,"select order_status.name from order_status,`order` where `order`.id='$order_id' and `order`.order_status=order_status.id"));
+							echo $order_status_arr['name'];
+							?>
+							
+							<div>
+								<form method="post">
+									<select class="form-control" name="update_order_status" required>
+										<option value="">Select Status</option>
+										<?php
+										$res=mysqli_query($con,"select * from order_status");
+										while($row=mysqli_fetch_assoc($res)){
+											if($row['id']==$categories_id){
+												echo "<option selected value=".$row['id'].">".$row['name']."</option>";
+											}else{
+												echo "<option value=".$row['id'].">".$row['name']."</option>";
+											}
+										}
+										?>
+									</select>
+									<input type="submit" class="form-control"/>
+								</form>
+							</div>
+						</div>
+				   </div>
+				</div>
+			 </div>
+		  </div>
+	   </div>
+	</div>
+</div>
+<?php
+require('footer.inc.php');
+?>
